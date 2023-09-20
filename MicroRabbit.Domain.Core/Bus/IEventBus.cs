c33ebc403e5fafa6506518240fd6f57c4b86f01f
Ahmed-Aboulbichr <aboulbichrahmed2@gmail.com>
2023-09-20 14:19:22 +0100

    implement of RabbitMQBus

--- MicroRabbit.Domain.Core/Bus/IEventBus.cs
+++ MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -1,4 +1,7 @@
-﻿namespace MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus;
 public interface IEventBus {
 
     Task SendCommand<T>(T command) where T : Command;
